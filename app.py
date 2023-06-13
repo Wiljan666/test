3db7c030a8c9424f16563f907d94f15918c4f8a1
Wiljan666 <136391627+Wiljan666@users.noreply.github.com>
2023-06-13 09:42:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,8 @@
 import streamlit as st
 
-# Initialize session state variables
-if 'toolnummer' not in st.session_state:
-    st.session_state.toolnummer = 12
-if 'startposx' not in st.session_state:
-    st.session_state.startposx = 13.8
-if 'startposz' not in st.session_state:
-    st.session_state.startposz = 2
-if 'eindposz' not in st.session_state:
-    st.session_state.eindposz = -99
-if 'besturing' not in st.session_state:
-    st.session_state.besturing = 'fanuc'
-if 'veiligafstand' not in st.session_state:
-    st.session_state.veiligafstand = 2.0
-if 'decimalen' not in st.session_state:
-    st.session_state.decimalen = '3'
-if 'mmvariatie' not in st.session_state:
-    st.session_state.mmvariatie= 2
-if 'rpmvariatie' not in st.session_state:
-    st.session_state.rpmvariatie=150
-if 'toerental' not in st.session_state:
-    st.session_state.toerental=1500
-if 'voeding' not in st.session_state:
-    st.session_state.voeding=0.1
-if 'veiligafstand' not in st.session_state:
-    st.session_state.veiligafstand=2
+user_credentials = [("jan", "123"), ("admin", "admin")]
 
-# UI layout
-st.set_page_config(layout="wide")
 
-# Colors
-venster_kleur = "darkgray"
-knoppen_kleur = "steelblue"
-tabview_kleur = "lightslategray"
-
-# Functions for generating NC-code
 def button_genereer():
     toolnummer = st.session_state.toolnummer
     gereedschap = str(toolnummer)
@@ -121,46 +89,118 @@ def button_genereer():
     st.write("Gegenereerde NC-code:")
     st.code(nc_code)
 
-def button_opslaan():
-    nc_code = st.session_state.nc_code
 
-    # Save the NC code to a file
-    with open("generated_code.txt", "w") as f:
-        f.write(nc_code)
+def login(username, password):
+    for user, pwd in user_credentials:
+        if user == username and pwd == password:
+            return True
+    return False
 
-    st.write("NC-code succesvol opgeslagen.")
+def main():
+    st.set_page_config(layout="wide")
+
+    if 'logged_in' not in st.session_state:
+        st.session_state.logged_in = False
+
+    if st.session_state.logged_in:
+        show_app()
+    else:
+        show_login()
+        
+        
+def show_login():
+    st.title("Inloggen")
+
+    if st.session_state.logged_in:
+        hide_login()
+        show_app()
+    else:
+        username = st.text_input("Gebruikersnaam")
+        password = st.text_input("Wachtwoord", type="password")
+
+        login_button = st.button("Inloggen")
+
+        if login_button:
+            if login(username, password):
+                st.session_state.logged_in = True
+                hide_login()
+                show_app()
+            else:
+                st.error("Ongeldige gebruikersnaam of wachtwoord")
+
+def hide_login():
+    # Verberg het inlogscherm
+    st.empty()
 
 
-# UI-componenten
-st.title('SSV-app')
 
-# Linkerkolom
-st.sidebar.title('Instellingen')
+def show_app():
+    if not st.session_state.logged_in:
+        show_login()
+        return
+    
+    # Initialize session state variables
+    if 'toolnummer' not in st.session_state:
+        st.session_state.toolnummer = 12
+    if 'startposx' not in st.session_state:
+        st.session_state.startposx = 13.8
+    if 'startposz' not in st.session_state:
+        st.session_state.startposz = 2
+    if 'eindposz' not in st.session_state:
+        st.session_state.eindposz = -99
+    if 'besturing' not in st.session_state:
+        st.session_state.besturing = 'fanuc'
+    if 'veiligafstand' not in st.session_state:
+        st.session_state.veiligafstand = 2.0
+    if 'decimalen' not in st.session_state:
+        st.session_state.decimalen = '3'
+    if 'mmvariatie' not in st.session_state:
+        st.session_state.mmvariatie= 2
+    if 'rpmvariatie' not in st.session_state:
+        st.session_state.rpmvariatie=150
+    if 'toerental' not in st.session_state:
+        st.session_state.toerental=1500
+    if 'voeding' not in st.session_state:
+        st.session_state.voeding=0.1
+    
 
-# Input fields
-st.sidebar.subheader('Invoer')
-toolnummer = st.sidebar.number_input('Toolnummer:', min_value=1, step=1, key='toolnummer')
-startposx = st.sidebar.number_input('Startpositie X:', value=0.0, step=0.1, key='startposx')
-startposz = st.sidebar.number_input('Startpositie Z:', value=0.0, step=0.1, key='startposz')
-eindposz = st.sidebar.number_input('Eindpositie Z:', value=0.0, step=0.1, key='eindposz')
-veiligafstand = st.sidebar.number_input('Veiligafstand:', value=2.0, step=0.1, key='veiligafstand')
-st.sidebar.subheader('Machine instellingen')
-besturing = st.sidebar.selectbox('Besturing:', ['fanuc', 'siemens'], index=0, key='besturing')
-decimalen = st.sidebar.selectbox('Decimalen:', ['1', '2', '3', '4'], index=0, key='decimalen')
-voeding = st.sidebar.number_input('Voeding:', value=0.0, step=0.1, key='voeding')
-rpmvariatie = st.sidebar.number_input('RPM-variatie:', value=0, step=1, key='rpmvariatie')
-toerental = st.sidebar.number_input('Basis RPM:', value=0, step=1, key='toerental')
-mmvariatie = st.sidebar.number_input('MM-variatie:', value=0.0, step=0.1, key='mmvariatie')
+    # Colors
+    venster_kleur = "darkgray"
+    knoppen_kleur = "steelblue"
+    tabview_kleur = "lightslategray"
 
-# Genereer- en Opslaanknoppen
-if st.sidebar.button('Genereer', on_click=button_genereer):
-    pass
+    # Functions for generating NC-code
 
+    # UI-componenten
+    st.title('SSV-app')
 
-# Rechterkolom
-st.write('Druk links onderin op genereer en de NC-code verschijnt hier:')
+    # Linkerkolom
+    st.sidebar.title('Instellingen')
 
+    # Input fields
+    st.sidebar.subheader('Invoer')
+    toolnummer = st.sidebar.number_input('Toolnummer:', min_value=1, step=1, key='toolnummer')
+    startposx = st.sidebar.number_input('Startpositie X:', value=0.0, step=0.1, key='startposx')
+    startposz = st.sidebar.number_input('Startpositie Z:', value=0.0, step=0.1, key='startposz')
+    eindposz = st.sidebar.number_input('Eindpositie Z:', value=0.0, step=0.1, key='eindposz')
+    veiligafstand = st.sidebar.number_input('Veiligafstand:', value=2.0, step=0.1, key='veiligafstand')
+    st.sidebar.subheader('Machine instellingen')
+    besturing = st.sidebar.selectbox('Besturing:', ['fanuc', 'siemens'], index=0, key='besturing')
+    decimalen = st.sidebar.selectbox('Decimalen:', ['1', '2', '3', '4'], index=0, key='decimalen')
+    voeding = st.sidebar.number_input('Voeding:', value=0.0, step=0.1, key='voeding')
+    rpmvariatie = st.sidebar.number_input('RPM-variatie:', value=0, step=1, key='rpmvariatie')
+    toerental = st.sidebar.number_input('Toerental:', value=0, step=1, key='toerental')
+    mmvariatie = st.sidebar.number_input('MM-variatie:', value=0.0, step=0.1, key='mmvariatie')
 
+    # Genereer- en Opslaanknoppen
+    if st.sidebar.button('Genereer', on_click=button_genereer):
+        pass
 
-#streamlit run D:\Wiljan Privé\test\app.py
-#cd D:\Wiljan Privé\test chrome app
+    # Rechterkolom
+    st.write('Voeg hier je code voor de rechterkolom toe.')
+
+    # UI-componenten en logica van de app
+    # Voeg hier de rest van je app logica toe
+
+if __name__ == '__main__':
+    main()
